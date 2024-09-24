e2d77973ab7ac41147133f2766dd7c72b3bc5482
Kassandra Mije <kassandra.mije@supportzebra.com>
2024-09-24 16:33:49 +0800

    new updates

---
+++ src/components/pages/UserFeatures/Home/PublicationDetails.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+
+const PublicationDetails = () => {
+    return <Stack></Stack>;
+};
+
+export default PublicationDetails;
